3017663813338a5e4568b34a692eeea1b968a2a9
YuanHang <xdw3233045951@163.com>
2022-09-19 20:37:05 +0800

    整数拆解final

--- September/src/main/java/com/victer/September19th/mainTest.java
+++ September/src/main/java/com/victer/September19th/mainTest.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 
 public class mainTest {
     public static void main(String[] args) {
-        System.out.println(integerBreak(10));
+        System.out.println(integerBreak(3));
     }
     public static int integerBreak(int n) {
         if(n == 2){
@@ -13,15 +13,16 @@ public class mainTest {
         }
         int [] arr = new int[n + 1];
         arr[1] = 1;
-        arr[2] =2;
-        int begin = 3 , fin = n , tem = 0 ;
+        int begin = 2 , fin = n , tem = 0 , firstFigure = 0 , secondFigure = 0;
         for(;begin <= fin; begin++){
             for(int i = 1 ; i <= begin / 2 ; i++){
-                tem = tem > (arr[i] * arr[begin - i])? tem : (arr[i] * arr[begin - i]);
+                firstFigure = i > arr[i] ? i: arr[i];
+                secondFigure = (begin - i) > arr[begin - i] ? (begin - i) : arr[begin - i];
+                tem = tem > firstFigure * secondFigure? tem : firstFigure* secondFigure;
             }
             arr[begin] = tem;
         }
-+
+
         return arr[n];
     }
 }
